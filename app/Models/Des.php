e348089687c8389d17036cc52c1e0ca976f126c5
weynard02 <samsicoweynard@gmail.com>
2023-10-03 17:10:30 +0700

    feat: make relationship

--- app/Models/Des.php
+++ app/Models/Des.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Des extends Model
 {
     use HasFactory;
 
     protected $fillable = ['fullname', 'id_card', 'document', 'video'];
+
+    public function user(): BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -41,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function aes(): HasOne 
+    {
+        return $this->hasOne(Aes::class);
+    }
 }
