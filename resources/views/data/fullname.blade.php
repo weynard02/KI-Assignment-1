1945692e786db09f467acfa4b00cfc62f9fd9600
athraz <athrazan2004@gmail.com>
2023-11-20 19:26:34 +0700

    feat: submit symmetric key

--- resources/views/data/fullname.blade.php
+++ resources/views/data/fullname.blade.php
@@ -1,5 +1,9 @@
 @extends('master')
 @include('navbar')
+@php
+    $homeController = app('App\Http\Controllers\HomeController');
+    $aes = $aess->first();
+@endphp
 @section('content')
 <div class="container">
     <div class="text-center mb-5" style="margin-top: 100px">
@@ -15,19 +19,17 @@
             <div class="card-body">
                 <div class="d-flex flex-column">
                     <div class="form-group mb-3 p-2">
-                        <div class="form-group mb-3 p-2">
-                            <label class="fw-bold mb-3" style="font-size: 20px;">Decrypt key from your email</label>
-                            <textarea id="encsymkey" rows="5" class="form-control" name="encsymkey" placeholder="Enter the key from your email" value=""></textarea>
-                        </div>
+                        <label class="fw-bold mb-3" style="font-size: 20px;">Decrypt key from your email</label>
+                        <textarea id="encsymkey" rows="5" class="form-control" name="encsymkey" placeholder="Enter the key from your email" value=""></textarea>
+                    </div>
 
-                        <div class="d-flex justify-content-end mb-5">
-                            <button id="submitButton" class="btn btn-dark mx-2" type="submit">Submit</button>
-                        </div>
+                    <div class="d-flex justify-content-end mb-5">
+                        <button id="submitButton1" class="btn btn-dark mx-2" type="submit">Submit</button>
+                    </div>
 
-                        <div class="mb-3 p-2">
-                            <label class="fw-bold mb-3" style="font-size: 20px;">Here is your symmetric key</label>
-                            <textarea id="outputTextarea" class="form-control" rows="2" readonly></textarea>
-                        </div>
+                    <div class="mb-3 p-2">
+                        <label class="fw-bold mb-3" style="font-size: 20px;">Here is your symmetric key</label>
+                        <textarea id="outputTextarea" class="form-control" rows="2" readonly></textarea>
                     </div>
                 </div>
             </div>
@@ -35,27 +37,26 @@
         <div class="card mb-3 col-lg-5 mx-3">
             <div class="card-body">
                 <div class="d-flex flex-column">
-                    <form action="/home" method="post" enctype="multipart/form-data">
-                        <div class="form-group mb-3 p-2">
-                            <label class="fw-bold mb-3" style="font-size: 20px;">Symmetric key</label>
-                            <input type="text" class="form-control" name="symkey" placeholder="Enter the symmetric key" value="">
-                            @error('symkey')
-                            <div class="alert alert-danger fs-6 text">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="d-flex justify-content-end mb-5">
-                            <button class="btn btn-dark mx-2" type="submit">Submit</button>
-                        </div>
-                    </form>
+                    <div class="form-group mb-3 p-2">
+                        <label class="fw-bold mb-3" style="font-size: 20px;">Symmetric Key</label>
+                        <input type="hidden" class="form-control" id="realsymkey" value="{{$aesuser->fullname_key}}">
+                        <textarea id="symkey" rows="5" class="form-control" name="symkey" placeholder="Enter the symmetric key" value=""></textarea>
+                    </div>
+
+                    <div class="d-flex justify-content-end mb-5">
+                        <button id="submitButton2" class="btn btn-dark mx-2" type="submit">Submit</button>
+                    </div>
+
                     <form action="/home" method="post" enctype="multipart/form-data">
                         <div class="form-group mb-5 p-2 d-flex justify-content-between align-items-center">
                             <label class="fw-bold mb-3" style="font-size: 20px;">Not requested yet?</label>
-                            <input type="hidden" class="form-control" name="symkey" placeholder="Enter the symmetric key" value="">
                             <button class="btn btn-dark" type="submit">Request</button>
                         </div>
                     </form>
-                    <div class="form-group mb-3 p-2 d-flex justify-content-between align-items-center">
-                        <label class="fw-bold mb-3" style="font-size: 20px;">Here is {{$user->username}}'s fullname</label>
+
+                    <div class="form-group mb-2 p-2 d-block visually-hidden" id="hiddendata">
+                        <label class="fw-bold mb-2" style="font-size: 20px;">Here is {{$user->username}}'s fullname</label>
+                        <p class="text-muted">{{ $homeController->AESdecrypt($aes->fullname, $aes->fullname_key, $aes->fullname_iv, 0) }}</p>
                     </div>
                 </div>
             </div>
@@ -64,7 +65,7 @@
 </div>
 
 <script>
-    document.getElementById('submitButton').addEventListener('click', function() {
+    document.getElementById('submitButton1').addEventListener('click', function() {
         var inputValue = document.getElementById('encsymkey').value;
 
         if (inputValue.trim() !== '') {
@@ -83,6 +84,23 @@
                     console.log(error);
                 }
             });
+        }
+    });
+
+    document.getElementById('submitButton2').addEventListener('click', function() {
+        var inputValue = document.getElementById('symkey').value;
+        var realsymkey = document.getElementById('realsymkey').value;
+        var hiddenDataDiv = document.getElementById('hiddendata');
+
+        if (inputValue == realsymkey) {
+            if (hiddenDataDiv.classList.contains('visually-hidden')) {
+                hiddenDataDiv.classList.remove('visually-hidden');
+            }
+        }
+        else {
+            if (!hiddenDataDiv.classList.contains('visually-hidden')) {
+                hiddenDataDiv.classList.add('visually-hidden');
+            }
         }
     });
 </script>
